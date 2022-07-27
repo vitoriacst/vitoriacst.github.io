78539f589b66e391e89532293df554e964b4931f
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-07-27 20:17:56 -0300

    feat: :sparkles: HomePage

--- app/src/pages/HomePage.jsx
+++ app/src/pages/HomePage.jsx
@@ -1,8 +1,14 @@
+import computer from '../images/computer.svg';
 const HomePage = () => {
   return (
-    <div>
-      <div>
-        <h1 className="text-yellow-200">Vitoria Holanda</h1>
+    <div className="flex items-center justify-center">
+      <div className="items-center  justify-center mt-72 flex flex-col">
+        <img src={computer} alt="computer " className="mb-10" />
+        <h1 className="text-white font-bold">VITÓRIA HOLANDA</h1>
+        <h1 className="text-white font-extralight">Dev Front-end</h1>
+        <button className="bg-transparent hover:bg-blue-900 text-white font-semibold hover:text-white py-2 px-4 border border-white hover:border-transparent rounded mt-8">
+          SOBRE MIM
+        </button>
       </div>
     </div>
   );
