3c7d88fec05bbb5a9f8ec6b58f64168464361535
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-07-27 20:58:48 -0300

    feat: :sparkles: details screen settings about me

--- app/src/pages/AboutMe.jsx
+++ app/src/pages/AboutMe.jsx
@@ -1,18 +1,9 @@
-import MyPicture from '../images/myProfile.jpeg';
 const AboutMe = () => {
   return (
-    <div className="mflex items-center justify-center">
-      <div className="mt-10 flex flex-col gap-10 justify-center items-center">
-        <div className="">
-          <img
-            src={MyPicture}
-            className="mb-3 w-56 h-56 rounded-full shadow-lg"
-            alt="vitoria"
-          />
-        </div>
-      </div>
+    <div className="flex items-center flex-col justify-center">
+      <div className="mt-56 flex flex-col gap-10 justify-center items-center"></div>
       <div className="ml-8">
-        <h1 className="text-white font-bold">Sobre mim</h1>
+        <h1 className="text-white font-bold">SOBRE MIM</h1>
         <p className="text-white font-light">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto
           dolorem aliquam consequatur praesentium aliquid optio ullam nulla quo
@@ -20,12 +11,29 @@ const AboutMe = () => {
           ratione est aperiam quod.
         </p>
       </div>
-      <div className=" flex justify-center flex-col items-center mt-10 p-2">
-        <h1 className="text-white font-bold">Minhas Skills</h1>
+      <div className=" flex justify-center flex-col items-center mt-10 gap-2 p-2">
+        <h1 className="text-white font-bold">MINHAS SKILLS</h1>
         <div className="bg-transparent border border-white p-4 text-white rounded-md ">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et quasi
-          ratione impedit, nostrum veniam dicta pariatur nobis libero quod, hic
-          accusantium id sunt ut? Sequi eum tempore consequuntur aut quos.
+          <ul className="flex gap-2 flex-col">
+            <li>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et quasi
+              ratione impedit, nostrum veniam dicta pariatur nobis libero quod,
+              hic accusantium id sunt ut? Sequi eum tempore consequuntur aut
+              quos.
+            </li>
+            <li>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et quasi
+              ratione impedit, nostrum veniam dicta pariatur nobis libero quod,
+              hic accusantium id sunt ut? Sequi eum tempore consequuntur aut
+              quos.
+            </li>
+            <li>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et quasi
+              ratione impedit, nostrum veniam dicta pariatur nobis libero quod,
+              hic accusantium id sunt ut? Sequi eum tempore consequuntur aut
+              quos.
+            </li>
+          </ul>
         </div>
       </div>
     </div>
